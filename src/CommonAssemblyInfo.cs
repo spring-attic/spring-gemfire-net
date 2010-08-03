8fb92a29d35f6f8a6a5f680d95d76f596eff9d19
markpollack <mark.pollack@springsource.com>
2010-08-03 00:15:47 -0400

    changes to support ndoc instead of Document X! for API doc generation switch batch in 3.0.0.9 dlls

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -22,8 +22,8 @@ using System.Security.Permissions;
 [assembly: AssemblyCopyrightAttribute("Copyright 2010 SpringSource.")]
 [assembly: AssemblyTrademarkAttribute("Apache License, Version 2.0")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("1.0.0.3863")]
+[assembly: AssemblyVersionAttribute("1.0.0.3867")]
 [assembly: AssemblyConfigurationAttribute("net-3.5.win32; dev")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0.3863; net-3.5.win32; dev")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.3867; net-3.5.win32; dev")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
