70d8669de8f72efa761e69ac62800dceaba0ee7d
Mark Pollack <mark.pollack@springsource.com>
2010-12-22 12:26:11 +0000

    SGFNET-16 - Update to Spring.NET 1.3.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -22,8 +22,8 @@ using System.Security.Permissions;
 [assembly: AssemblyCopyrightAttribute("Copyright 2010 SpringSource.")]
 [assembly: AssemblyTrademarkAttribute("Apache License, Version 2.0")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("1.0.0.341")]
-[assembly: AssemblyConfigurationAttribute("net-3.5.win32; release")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0.341; net-3.5.win32; release")]
+[assembly: AssemblyVersionAttribute("1.0.0.355")]
+[assembly: AssemblyConfigurationAttribute("net-3.5.win32; dev")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.355; net-3.5.win32; dev")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
